4c86fe9597eb99dd491bc53646675c784791d027
omgzui <862275679@qq.com>
2021-12-17 15:42:03 +0800

    栈+map

--- src/main/java/com/e/dxy/leetcode/easy/stack/ValidParentheses20.java
+++ src/main/java/com/e/dxy/leetcode/easy/stack/ValidParentheses20.java
@@ -1,17 +1,43 @@
 package com.e.dxy.leetcode.easy.stack;
 
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 public class ValidParentheses20 {
 
-    public static boolean isValid(String s) {
+    public static boolean isValid111(String s) {
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
             if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
             } else {
                 if (!stack.isEmpty() && stackOf(c) == stack.peek()) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.empty();
+    }
+
+    public static boolean isValid222(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        // 等同于stackOf()
+        Map<Character, Character> stackOf  = new HashMap<>();
+        stackOf.put(')', '(');
+        stackOf.put('}', '{');
+        stackOf.put(']', '[');
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (!stack.isEmpty() && stackOf.get(c) == stack.peek()) {
                     stack.pop();
                 } else {
                     return false;
@@ -37,8 +63,8 @@ public class ValidParentheses20 {
         // s = "()[]{}"
         String s1 = "()[]{}";
         String s2 = "(){]{}";
-        Boolean isValid1 = isValid(s1);
-        Boolean isValid2 = isValid(s2);
+        Boolean isValid1 = isValid111(s1);
+        Boolean isValid2 = isValid222(s2);
         System.out.println(isValid1);
         System.out.println(isValid2);
     }
